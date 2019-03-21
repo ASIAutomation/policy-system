b89a672993bcdf95fe7e396b17a593f1577c651f
dcohen <dcohen@D572R1G2.asicorp.org>
2019-03-21 16:12:20 -0400

    test

--- src/test/java/Automation/PS/ThirdPartyCheck/BrushfireTest.java
+++ src/test/java/Automation/PS/ThirdPartyCheck/BrushfireTest.java
@@ -1,21 +1,17 @@
 package Automation.PS.ThirdPartyCheck;
 
 import java.util.concurrent.TimeUnit;
-
 import org.apache.log4j.Logger;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
-
 import Automation.PS.pages.PlanAndResidenceType;
 import Automation.PS.pages.PolicyRatingPage1;
 import Automation.PS.pages.RatingAndPolicyLookup;
